2c667cd94b6b0d6fc80368f8c03ae379f7158f23
palashichevaa@mail.ru <palashichevaa@mail.ru>
2022-10-01 11:02:48 +1000

    финал

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -64,72 +64,79 @@ namespace ISRPO_Palashicheva_PR3._1
 
         private void pp3_Click(object sender, RoutedEventArgs e)
         {
-            a = float.Parse(tb1.Text);
-            tb1.Text = Convert.ToString(a + 3);
+            a = float.Parse(tb2.Text);
+            tb2.Text = Convert.ToString(a + 3);
         }
 
         private void pp2_Click(object sender, RoutedEventArgs e)
         {
-            a = float.Parse(tb1.Text);
-            tb1.Text = Convert.ToString(a + 2);
+            a = float.Parse(tb2.Text);
+            tb2.Text = Convert.ToString(a + 2);
         }
         private void pp1_Click(object sender, RoutedEventArgs e)
         {
-            a = float.Parse(tb1.Text);
-            tb1.Text = Convert.ToString(a + 1);
+            a = float.Parse(tb2.Text);
+            tb2.Text = Convert.ToString(a + 1);
         }
 
         private void mm1_Click(object sender, RoutedEventArgs e)
         {
-            a = float.Parse(tb1.Text);
-            tb1.Text = Convert.ToString(a - 1);
+            a = float.Parse(tb2.Text);
+            tb2.Text = Convert.ToString(a - 1);
         }
 
         private void mm2_Click(object sender, RoutedEventArgs e)
         {
-            a = float.Parse(tb1.Text);
-            tb1.Text = Convert.ToString(a - 2);
+            a = float.Parse(tb2.Text);
+            tb2.Text = Convert.ToString(a - 2);
         }
 
         private void mm3_Click(object sender, RoutedEventArgs e)
         {
-            a = float.Parse(tb1.Text);
-            tb1.Text = Convert.ToString(a - 3);
+            a = float.Parse(tb2.Text);
+            tb2.Text = Convert.ToString(a - 3);
         }
 
         private void ppp3_Click(object sender, RoutedEventArgs e)
         {
-            a = float.Parse(tb1.Text);
-            tb1.Text = Convert.ToString(a + 3);
+            a = float.Parse(tb3.Text);
+            tb3.Text = Convert.ToString(a + 3);
         }
 
         private void ppp2_Click(object sender, RoutedEventArgs e)
         {
-            a = float.Parse(tb1.Text);
-            tb1.Text = Convert.ToString(a + 2);
+            a = float.Parse(tb3.Text);
+            tb3.Text = Convert.ToString(a + 2);
         }
         private void ppp1_Click(object sender, RoutedEventArgs e)
         {
-            a = float.Parse(tb1.Text);
-            tb1.Text = Convert.ToString(a + 1);
+            a = float.Parse(tb3.Text);
+            tb3.Text = Convert.ToString(a + 1);
         }
 
         private void mmm1_Click(object sender, RoutedEventArgs e)
         {
-            a = float.Parse(tb1.Text);
-            tb1.Text = Convert.ToString(a - 1);
+            a = float.Parse(tb3.Text);
+            tb3.Text = Convert.ToString(a - 1);
         }
 
         private void mmm2_Click(object sender, RoutedEventArgs e)
         {
-            a = float.Parse(tb1.Text);
-            tb1.Text = Convert.ToString(a - 2);
+            a = float.Parse(tb3.Text);
+            tb3.Text = Convert.ToString(a - 2);
         }
 
         private void mmm3_Click(object sender, RoutedEventArgs e)
         {
-            a = float.Parse(tb1.Text);
-            tb1.Text = Convert.ToString(a - 3);
+            a = float.Parse(tb3.Text);
+            tb3.Text = Convert.ToString(a - 3);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tb1.Text = "3";
+            tb2.Text = "2";
+            tb3.Text = "1";
         }
     }
 }
